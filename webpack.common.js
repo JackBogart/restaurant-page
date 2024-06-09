275b07dad180ae08dcbe74e5d05213434962297f
Jack Bogart <jackbogart0822@gmail.com>
2024-06-09 19:29:04 -0400

    Add CSS minifier and babel transpiling
    Added plugins and loaders from webpack to transpile the code and also
    minify the CSS for prod.

--- webpack.common.js
+++ webpack.common.js
@@ -9,16 +9,12 @@ module.exports = {
         }),
     ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
     module: {
         rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
