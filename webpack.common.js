4c6c13391fe4ec5d37cf5df52672cd75b0c43fa9
Jack Bogart <jackbogart0822@gmail.com>
2024-06-09 01:30:05 -0400

    Add homepage for restaurant
    Included a background image that spans the entire body along with aborts
    headline and a short description.

--- webpack.common.js
+++ webpack.common.js
@@ -9,8 +9,28 @@ module.exports = {
         }),
     ],
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
     },
 };
